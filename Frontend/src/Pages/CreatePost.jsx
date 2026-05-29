625c0ec3e443eed773ad5efaae750c6362a8e2a7
ayushzAIML <theayushchakraborty@gmail.com>
2026-05-29 21:16:45 +0530

    fr

--- Frontend/src/Pages/CreatePost.jsx
+++ Frontend/src/Pages/CreatePost.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import axios from 'axios'
 
 function CreatePost() {
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+
+    const formData = new FormData(event.target)
+    axios.post("http://localhost:3000/create-post", formData)
+      .then((res) => {
+        alert("Post created successfully")
+        console.log(res)
+      })
+      .catch((err) => {
+        console.log(err)
+        alert("Error creating post")
+      })
+  }
+
   return (
     <section className='create-post-section'>
         <h1>Create Post</h1>
 
-        <form >
+        <form onSubmit={handleSubmit}>
             <input type="file" name='image' accept='image/*'/>
             <input type="text" name='caption' placeholder='enter caption' required />
             <button type='submit'> Submit</button>
--- Frontend/src/Pages/Feed.jsx
+++ Frontend/src/Pages/Feed.jsx
@@ -1,27 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Feed=()=> {
 
-  const [ post, setPosts ] = useState([
+  const [ posts, setPosts ] = useState([
     {
       _id: 1,
       image: "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGFuZGF8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
       caption: "Beautiful view of the mountains!"
     }
   ])
+
+  useEffect(()=>{
+    axios.get("http://localhost:3000/api/posts")
+    .then((res)=>{
+      setPosts(res.data.posts)
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  },[])
+
+
   return (
     <section className='feed-section'>
       <h1>Feed</h1>
+       {
+          posts.length>0?(
+            posts.map((post) => (
+            <div key={post._id} className='post-card'>
+              <img src={post.image} alt={post.caption} />
+              <p>{post.caption}</p>
+            </div>
+          ))
+        ):(
+          <h1>No posts available</h1>
+        )
 
-      <div className='posts'>
-        {post.map((post) => (
-          <div key={post._id} className='post-card'>
-            <img src={post.image} alt={post.caption} />
-            <p>{post.caption}</p>
-          </div>
-        ))}
-      </div>
-
+      }
     </section>
   )
 }
